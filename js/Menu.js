8678fc32ea188cf5908c342a49bc32ebc60da855
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-17 14:12:59 +0000

    refactoring: abstract menu method

--- js/Menu.js
+++ js/Menu.js
@@ -1,54 +1,86 @@
 window.onload = function(){
    
-    data = ["public/images/menu/breakfast.jpg", "15.5€", "Desayuno de la Casa", "Huevos, pancakes y tocino"];
-    food = document.getElementById('food');
+    const data = ["public/images/menu/breakfast.jpg", "15.5€", "Desayuno de la Casa", "Huevos, pancakes y tocino"];
+    const info = ["public/images/menu/pasta.jpg", "17.5€", "Pasta de la Casa", "Pasta, pesto y tocino"];
+    const infoFood = ["public/images/menu/salad.jpg", "17.5€", "Salad de la Casa", "Tomato, pesto y tocino"];
 
-    const image = document.createElement('img');
-    image.src = data[0];
-    food.appendChild(image);
-
-    const price = document.createElement('p');
-    price.className = "prices";
-    price.innerHTML = data[1];
-
-    const description = document.createElement('div');
-    description.className = "description";
-    description.appendChild(price);
+    const menu = document.getElementById('menu');
     
+    createMenu(data);
+    createMenu(info);
+    createMenu(infoFood);
     
-    const title = document.createElement('h5');
-    title.className = "product";
-    title.innerHTML = data[2];
-    description.appendChild(title);
-    
-    const product = document.createElement('h6');
-    product.innerHTML = data[3]
-    description.appendChild(product);
-    
-    food.appendChild(description);
-   
-    const counter = document.createElement('section');
-    counter.className = "counter";
-
-    const add = document.createElement('button');
-    add.className = "add";
-    add.innerHTML = "+";
-    counter.appendChild(add);
-
-    const count = document.createElement('p');
-    count.className = "quantity";
-    count.innerHTML = 0;
-    counter.appendChild(count);
-
-    const rest = document.createElement('button');
-    rest.className = "rest";
-    rest.innerHTML = "-";
-    counter.appendChild(rest);
-
-    food.appendChild(counter);
-           
-    
+    function createMenu(data){       
+        const food = document.createElement('div');  
+        food.className = 'food';  
+        const description = document.createElement('div');
+        description.className = "description";
+        const counter = document.createElement('section');
+        setImage(data[0], food);
+        setPrice(data[1], description);
+        setTitle(data[2], description);
+        setProduct(data[3],food, description);
+        setCounter(food, counter);
+        menu.appendChild(food);
     }
+
+    function setImage(img, div){
+        const image = document.createElement('img');
+        image.src = img;
+        div.appendChild(image); 
+    }
+
+    function setPrice(value, div){
+        const price = document.createElement('p');
+        price.className = "prices";
+        price.innerHTML = data[1];
+        div.appendChild(price);
+    }
+    
+    function setTitle(valueTitle, div){
+        const title = document.createElement('h5');
+        title.className = "product";
+        title.innerHTML = valueTitle;
+        div.appendChild(title);
+    }
+
+    function setProduct(valueProduct, div, description){
+        const product = document.createElement('h6');
+        product.innerHTML = valueProduct;
+        description.appendChild(product);
+        div.appendChild(description);
+    }
+   
+    
+    function setCounter(div, counter){
+        counter.className = "counter";
+        setAdd(counter);
+        setAmount(counter);
+        setRest(counter);
+        div.appendChild(counter);
+    }
+
+    function setAdd(counter){
+        const add = document.createElement('button');
+        add.className = "add";
+        add.innerHTML = "+";
+        counter.appendChild(add);
+    }
+
+    function setAmount(counter){
+        const count = document.createElement('p');
+        count.className = "quantity";
+        count.innerHTML = 0;
+        counter.appendChild(count);
+    }
+    function setRest(counter){
+        const rest = document.createElement('button');
+        rest.className = "rest";
+        rest.innerHTML = "-";
+        counter.appendChild(rest);
+    }
+                
+}
 
 
 

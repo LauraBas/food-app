d7abe62b9c84872cb5bb3115d66b1723ef628999
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-18 09:22:01 +0000

    export data menu into menu

--- js/DataMenu.js
+++ js/DataMenu.js
@@ -1,24 +1,27 @@
-data = [
-    {
-        image:"public/images/menu/breakfast.jpg",
-        price:"15.5€",
-        title:"Desayuno de la Casa",
-        description:"Huevos, pancakes y tocino",
-        id: 1,
-    },
-    {
-        image:"public/images/menu/pasta.jpg",
-        price:"18.5€",
-        title:"Pasta de la Casa",
-        description:"Pasta, tomate y albhaca",
-        id: 2,
-    },
-    {
-        image:"public/images/menu/salad.jpg",
-        price:"12.5€",
-        title:"Salad de la Casa",
-        description:"Lechuga, tomate y cebolla",
-        id: 3,
-    },
 
-]
+export const dataMenu = [
+        {
+            image:"public/images/menu/breakfast.jpg",
+            price:"15.5€",
+            title:"Desayuno de la Casa",
+            description:"Huevos, pancakes y tocino",
+            id: 1,
+        },
+        {
+            image:"public/images/menu/pasta.jpg",
+            price:"18.5€",
+            title:"Pasta de la Casa",
+            description:"Pasta, tomate y albhaca",
+            id: 2,
+        },
+        {
+            image:"public/images/menu/salad.jpg",
+            price:"12.5€",
+            title:"Salad de la Casa",
+            description:"Lechuga, tomate y cebolla",
+            id: 3,
+        },
+    ];
+
+    
+
--- js/Menu.js
+++ js/Menu.js
@@ -1,33 +1,10 @@
-window.onload = function(){
-    const menu = document.getElementById('menu');
-   
-    dataMenu = [
-        {
-            image:"public/images/menu/breakfast.jpg",
-            price:"15.5€",
-            title:"Desayuno de la Casa",
-            description:"Huevos, pancakes y tocino",
-            id: 1,
-        },
-        {
-            image:"public/images/menu/pasta.jpg",
-            price:"18.5€",
-            title:"Pasta de la Casa",
-            description:"Pasta, tomate y albhaca",
-            id: 2,
-        },
-        {
-            image:"public/images/menu/salad.jpg",
-            price:"12.5€",
-            title:"Salad de la Casa",
-            description:"Lechuga, tomate y cebolla",
-            id: 3,
-        },
-    
-    ]
+import { dataMenu } from "./DataMenu.js"  ;
 
-    dataMenu.forEach(data => { createMenu(data) });
+window.onload = function(){
+    const menu = document.getElementById('menu'); 
     
+    dataMenu.forEach(data => { createMenu(data) });
+
     function createMenu(data){       
         const food = document.createElement('div');  
         food.className = 'food';  

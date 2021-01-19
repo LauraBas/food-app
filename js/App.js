f040e77a5ec4279719dc4e08a63055f32523f004
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-19 20:10:20 +0000

    Delete unused methods

--- js/App.js
+++ js/App.js
@@ -1,25 +1,8 @@
 'use strict';
-document.getElementById('plus').addEventListener('click',sum);
-document.getElementById('substract').addEventListener('click', substract);
+
 class App{
 
-   
 
- counter = 0;    
-
-    sum(){
-        counter++;
-        console.log(counter);
-        document.getElementById('counter').innerHTML = counter;
-    }
-
-
-    substract(){
-        if (counter > 0){
-            counter--;
-        }
-        document.getElementById('counter').innerHTML = counter;
-    }
   
 }
 

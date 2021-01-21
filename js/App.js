07dafee3989d6ad0eafd295dfddcdbdb2f553012
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-21 08:47:23 +0000

    Added checkout component

--- js/App.js
+++ js/App.js
@@ -1,10 +1,28 @@
 'use strict';
 
-class App{
+const cart = document.getElementById('cart');
+cart.addEventListener('click', change);
 
+const buttonBack = document.getElementById('return');
+buttonBack.addEventListener('click', change);
+
+
+function cleanScreen(){
+    const principal = document.getElementById('principal');
+    principal.style.display = (principal.style.display === "block") ? "none" : "block" ;
+}
+
+function changeScreen(){
+    const checkout = document.getElementById('checkout');
+    checkout.style.display = (checkout.style.display === "block") ? "none" : "block";
+}
+
+function change(){
+    cleanScreen();
+    changeScreen();
+}
 
   
-}
 
 
 

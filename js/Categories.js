d53eb5652c7a125d3e9ed8f70b11b9625f07edb9
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-23 17:14:38 +0000

    refactoring: rename category

--- js/Categories.js
+++ js/Categories.js
@@ -4,16 +4,16 @@ window.addEventListener ('load', function(){
 
     const slider = document.getElementById('slider'); 
     
-    categories.forEach(categorie => {print(categorie)});
+    categories.forEach(category => {print(category)});
 
-    function print(categorie){
+    function print(category){
         const div = document.createElement('article');
         div.className = "card-slider";
         const image = document.createElement('img');
-        image.src = categorie.image;
+        image.src = category.image;
         div.appendChild(image);
         const name = document.createElement('p');
-        name.innerHTML = categorie.name;
+        name.innerHTML = category.name;
         div.appendChild(name);
         slider.appendChild(div);
 
